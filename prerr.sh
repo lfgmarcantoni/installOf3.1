142834a75d09bad888572215fa9ec801b4d5e505
lfgmarcantoni <lfgmarcantoni@gmail.com>
2014-09-09 14:34:23 -0300

    Instalacion of3.1

---
+++ prerr.sh
@@ -0,0 +1,31 @@
+sudo apt-get update
+
+sudo apt-get install git-core 
+
+sudo apt-get install build-essential
+
+sudo apt-get install binutils-dev
+
+sudo apt-get install cmake 
+
+sudo apt-get install flex 
+
+sudo apt-get install bison 
+
+sudo apt-get install zlib1g-dev
+
+sudo apt-get install qt4-dev-tools
+
+sudo apt-get install libqt4-dev
+
+sudo apt-get install libncurses5-dev
+
+sudo apt-get install libiberty-dev
+
+sudo apt-get install libxt-dev
+
+sudo apt-get install rpm
+
+sudo apt-get install mercurial
+
+sudo apt-get install graphviz
